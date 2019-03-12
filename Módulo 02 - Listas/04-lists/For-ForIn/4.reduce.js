58e5752d6d48f1b2511000f726c0e93d44ec3755
Fábio Silva <fsilva@dcc.local>
2019-03-12 14:38:31 -0300

    Exercício reduce (com bug no segundo exemplo)

---
+++ Módulo 02 - Listas/04-lists/For-ForIn/4.reduce.js
@@ -0,0 +1,43 @@
+const { obterPessoas } = require('./service');
+
+Array.prototype.meuReduce = function(callback, valorInicial) {
+    let valorFinal = typeof valorInicial !== undefined ? valorInicial : this[0];
+    for (let index; index <= this.length -1; index ++) {
+        valorFinal = callback(valorFinal, this[index], this);
+    }
+    return valorFinal;
+}
+
+async function main() {
+    try {
+        const { results } = await obterPessoas(`a`);
+
+        //1º exemplo
+        // const pesos = results.map(item => parseInt(item.height));
+        // console.log('Pesos',pesos)
+        // const total = pesos.reduce((anterior, proximo) => {
+        //     return anterior + proximo
+        // }, 0);
+        // console.log('Total',total)
+
+
+        //2º exemplo
+        const minhaLista = [
+            ['Fabio','Jordão'],
+            ['NodeBR','Nerdzão']
+        ];
+
+        const listaReduzida = minhaLista.meuReduce((anterior, proximo) => {
+            return anterior.concat(proximo);
+        }, []).join(', ');
+
+        console.log('Lista reduzida', listaReduzida);
+
+
+    }
+    catch(erro) {
+        console.error('Deu ruim', erro);
+    }
+}
+
+main();
