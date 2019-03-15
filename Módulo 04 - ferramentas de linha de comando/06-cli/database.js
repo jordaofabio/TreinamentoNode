6f5cc23605430107196e017b50b881f441feea5c
unknown <fabiosjordao@gmail.com>
2019-03-15 01:25:49 -0300

    Mais exercícios do módulo 4

--- Módulo 04 - ferramentas de linha de comando/06-cli/database.js
+++ Módulo 04 - ferramentas de linha de comando/06-cli/database.js
@@ -1,7 +1,8 @@
-const { readFile } = require('fs');
+const { readFile, writeFile } = require('fs');
 const { promisify } = require('util');
 
 const readFileAsync = promisify(readFile);
+const writeFileAsync = promisify(writeFile);
 
 // Outra forma de obter dados do json
 // const dadosJson = require('./herois.json')
@@ -14,17 +15,28 @@ class Database {
 
     async obterAquivos() {
         const arquivo = await readFileAsync(this.NOME_ARQUIVO, 'utf8');
-        return JSON.parse(arquivo.toString());
+        return arquivo ? JSON.parse(arquivo.toString()) : arquivo;
     }
 
-    escreverArquivos() {
+    async escreverArquivos(dados) {
+        await writeFileAsync(this.NOME_ARQUIVO, JSON.stringify(dados));
+        return true;
+    }
 
+    async cadastrar(heroi) {
+        const dados = await this.obterAquivos();
+        const id = heroi.id <= 2 ? heroi.id : Date.now();
+        const heroiComId = { id, ...heroi };
+        const dadosFinal = [ ...dados, heroiComId ];
+        const resultado = await this.escreverArquivos(dadosFinal);
+
+        return resultado;
     }
 
     async listar(id) {
         const dados = await this.obterAquivos();
         // Se não tiver id, traz a lista completa
-        const dadosFilstrados = dados.filter(item => id ? item.id === id : true);
+        const dadosFilstrados = dados ? dados.filter(item => id ? item.id === id : true) : dados;
         return dadosFilstrados;
     }
 }
--- Módulo 04 - ferramentas de linha de comando/06-cli/test.js
+++ Módulo 04 - ferramentas de linha de comando/06-cli/test.js
@@ -7,14 +7,52 @@ const DEFAULT_ITEM_CADASTRAR = {
     id: 1
 }
 
-describe('Suite de manipulação de Heróis', () => {
+const DEFAULT_ITEM_ATUALIZAR = { 
+    nome: 'Lanterna Verde',
+    poder: 'Energia do Anel',
+    id: 2
+}
 
-    it('deve pesquisar um herói, usando aquirvos', async () => {
+
+describe('Suite de manipulação de Heróis', function() {
+
+    before(async () => {
+        const [lista] = await database.listar(null);
+        if(!lista) await database.cadastrar(DEFAULT_ITEM_CADASTRAR);
+    });
+
+    it('Deve pesquisar um herói, usando aquirvos', async () => {
         const expected = DEFAULT_ITEM_CADASTRAR;
         const [resultado] = await database.listar(expected.id);
-        console.log('Resultado', resultado);
-        console.log('expected', expected);
+
+        deepEqual(resultado, expected);
+    })
+
+    it('Deve cadastrar um heroi, usando arquivos', async () => {
+        const expeted = DEFAULT_ITEM_CADASTRAR;
+        const resultado = await database.cadastrar(DEFAULT_ITEM_CADASTRAR);
+        const [actual] = await database.listar(expeted.id);
+
+        deepEqual(actual, expeted);
+    });
+
+    it.only('Deve atualiza um heroi, pelo id', async () => {
         
+
+        const novoDado = {
+            "nome": "Batman",
+            "poder": "Dinheiro"
+        };
+
+        const expected = { 
+            ...DEFAULT_ITEM_ATUALIZAR,
+            ...novoDado
+        };
+
+        console.log('expected', expected);
+
+        const resultado = await database.atualizar(DEFAULT_ITEM_ATUALIZAR.id,novoDado);
+
         deepEqual(resultado, expected);
     })
 
