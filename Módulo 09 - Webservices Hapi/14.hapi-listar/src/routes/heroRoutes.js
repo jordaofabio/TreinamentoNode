b1c2b9c0ce0f718e7d5968fc553b9619aac035cc
Fabio Jordão <fabiosjordao@gmail.com>
2019-05-02 01:56:12 -0300

    Conclusão da aula 6 do módulo 09

--- Módulo 09 - Webservices Hapi/14.hapi-listar/src/routes/heroRoutes.js
+++ Módulo 09 - Webservices Hapi/14.hapi-listar/src/routes/heroRoutes.js
@@ -14,16 +14,28 @@ class HeroRoutes extends BaseRoute {
                 // o Hapi.js já resolve promise, então não precisamos de await
 
                 try {
-                    const { skip, limit, nome } = request.query;
+                    let { skip, limit, nome } = request.query;
 
-                    console.log('limit', parseInt(limit))
-                    console.log('skip', parseInt(skip))
+                    console.log('limit', limit)
+                    console.log('skip', skip)
+
+                    if (skip != undefined && isNaN(skip)) {
+                        throw Error('O tipo do skip deve ser number.');
+                    } else if (skip != undefined) {
+                        skip = parseInt(skip);
+                    }
+                    
+                    if (limit != undefined && isNaN(limit)) {
+                        throw Error('O tipo do limit deve ser number.');
+                    } else if (limit != undefined) {
+                        limit = parseInt(limit);
+                    }
 
                     let query = {}
                     if(nome) {
                         query.nome = nome;
                     }
-                    return this.db.read(query, parseInt(skip), parseInt(limit))
+                    return this.db.read(query, skip, limit)
 
                 }
                 catch (error) {
--- Módulo 09 - Webservices Hapi/14.hapi-listar/src/tests/apiHeroes.test.js
+++ Módulo 09 - Webservices Hapi/14.hapi-listar/src/tests/apiHeroes.test.js
@@ -30,7 +30,20 @@ describe.only('Suite de teste da API Heroes', function() {
         });
         const dados = JSON.parse(result.payload);
         const statusCode = result.statusCode;
-        assert.deepEqual(statusCode, 200)
-        assert.ok(dados.length === TAMANHO_LIMITE)
+        assert.deepEqual(statusCode, 200);
+        assert.ok(dados.length === TAMANHO_LIMITE);
+    });
+
+    it('Listar /herois - deve filtrar um item', async () => {
+        const TAMANHO_LIMITE = 10;
+        const NOME = 'Mulher Maravilha';
+        const result = await app.inject({
+            method: 'GET',
+            url: `/herois?skip=0&limit=${TAMANHO_LIMITE}&nome=${NOME}`
+        });
+        const dados = JSON.parse(result.payload);
+        const statusCode = result.statusCode;
+        assert.deepEqual(statusCode, 200);
+        assert.deepEqual(dados[0].nome, NOME);
     });
 })
